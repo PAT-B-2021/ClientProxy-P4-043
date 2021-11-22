c7c0cd9efc6065df8cd43115331a9187ea2a3216
annisaysfn <63139331+annisaysfn@users.noreply.github.com>
2021-11-22 11:41:30 +0700

    praktikum 6

--- ClientProxy-P4-043/Program.cs
+++ ClientProxy-P4-043/Program.cs
@@ -17,7 +17,7 @@ namespace ClientProxy_P4_043
                 double hasilTambah = obj.Tambah(1, 2);
                 Console.WriteLine("1 + 2 = " + hasilTambah);
                 double hasilKurang = obj.Kurang(3, 2);
-                Console.WriteLine("1 - 2 = " + hasilKurang);
+                Console.WriteLine("3 - 2 = " + hasilKurang);
                 double hasilKali = obj.Kali(2, 2);
                 Console.WriteLine("2 x 2 = " + hasilKali);
                 double hasilBagi = obj.Bagi(2, 0);
